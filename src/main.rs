a96f6e4d0640dbd94da1634585efbbf785b76e1c
@undeath-cyber <gabrielxjonathan.12@gmail.com>
2025-10-28 11:35:32 +0700

    gsghs

--- src/main.rs
+++ src/main.rs
@@ -30,3 +30,59 @@ fn shadowing_test() {
     let umur = 21;
     println!("Tahun depan umur saya adalah {}", umur);
 }
+
+#[test]
+fn augmented_test() {
+    let mut a = 10;
+    println!("{}", a);
+    a += 10;
+    println!("{}", a);
+    a *= 10;
+    println!("{}", a);
+    a %= 3;
+    println!("{}", a);
+}
+
+#[test]
+fn augmented_immutable_test() {
+    let a = 10;
+    println!("{}", a);
+    // a += 10; // ini jadi error
+    println!("{}", a);
+}
+
+#[test]
+fn comparison_test() {
+    let a = 10 <= 90;
+    println!("hasil variable a : {a}");
+}
+
+#[test]
+fn char_test() {
+    let a: char = 'a';
+    let b: char = 'b';
+    println!("hasil variable a : {a}");
+    println!("{} {}", a, b)
+}
+
+#[test]
+fn tuple_test() {
+    let a = (10, 2.3, false, 'a', "Hai");
+    println!("{:?}", a);
+    // println!("{}", a); // akan error
+
+    let angka = a.0;
+    println!("{}", angka);
+
+    let sapa = a.4;
+    println!("{}", sapa);
+}
+
+#[test]
+fn destructuring_tuple_test() {
+    let data = (100, -10, 3.0, 'a');
+    println!("{:?}", data);
+
+    let (a, b, c, _) = data;
+    println!("{} {} {}", a, b, c);
+}
